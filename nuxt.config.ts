99a3bbfd7cc99493108e626098c0216b0a934262
Liliia Bilous <liliyabilous.email@gmail.com>
2024-08-27 00:39:32 +0300

    replaced files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-      '@nuxthq/studio'
-    ]
-  })
-  
+  modules: [
+    '@nuxthq/studio'
+  ]
+})
