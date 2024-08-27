525ff653d7b2fe36fd841fa06725a93b8fe7b0af
Liliia Bilous <liliyabilous.email@gmail.com>
2024-08-27 12:29:25 +0300

    nuxt-content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxthq/studio'
-  ]
+  modules: ['@nuxthq/studio', '@nuxt/content']
 })
