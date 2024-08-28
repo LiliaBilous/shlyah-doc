c5e60e4f49aff3e9b1ab4f5322d61c3868f8d8e7
Liliia Bilous <liliyabilous.email@gmail.com>
2024-08-28 20:55:15 +0300

    new app config

--- content-app/app.config.ts
+++ content-app/app.config.ts
@@ -1,18 +1,58 @@
 export default defineAppConfig({
-  ui: {
-    primary: "blue",
-    gray: "slate"
-  },
-
-  header: {
-    links: ["/Home", "About"]
-  },
-
-  footer: {
-    colorMode: true
-  },
-
-  seo: {
-    siteName: "citeShyahDocs"
+  shadcnDocs: {
+    site: {
+      name: 'shlyah-docs-nuxt',
+      description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
+    },
+    theme: {
+      customizable: true,
+      color: 'blue',
+      radius: 0.5,
+    },
+    header: {
+      title: 'shlyah-docs-starter',
+      showTitle: true,
+      darkModeToggle: true,
+      logo: {
+        light: 'logo.svg',
+        dark: 'dark-theme-logo.svg',
+      },
+      nav: [],
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/LiliaBilous/shlyah-doc',
+        target: '_blank',
+      }],
+    },
+    aside: {
+      useLevel: true,
+      collapse: false,
+    },
+    main: {
+      breadCrumb: true,
+      showTitle: true,
+    },
+    footer: {
+      credits: 'Copyright © 2024',
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/LiliaBilous/shlyah-doc',
+        target: '_blank',
+      }],
+    },
+    toc: {
+      enable: true,
+      title: 'On This Page',
+      links: [{
+        title: 'Star on GitHub',
+        icon: 'lucide:star',
+        to: 'https://github.com/LiliaBilous/shlyah-doc',
+        target: '_blank',
+      }],
+    },
+    search: {
+      enable: true,
+      inAside: false,
+    }
   }
-})
+});
